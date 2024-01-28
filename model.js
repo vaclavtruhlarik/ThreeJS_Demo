f6adff5daf9dae544db9fa6c7fee60f3bca98096
vaclavtruhlarik <truhlarik.v@seznam.cz>
2024-01-28 22:11:16 +0100

    Model loading done right with lights and mose control

--- model.js
+++ model.js
@@ -1,33 +1,85 @@
 import * as THREE from "three";
 import WebGL from "three/addons/capabilities/WebGL.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 if (WebGL.isWebGLAvailable()) {
+    const clock = new THREE.Clock();
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-    camera.position.set(0, 0, 100);
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
+    camera.position.set(0, 0, 5);
     camera.lookAt(0, 0, 0);
 
     const scene = new THREE.Scene();
 
-    const loader = new GLTFLoader();
+    let object;
+    let mixer;
+    let controls;
+    let obj_to_render = "ship_in_a_bottle";
 
+    // Load the object
+    const loader = new GLTFLoader();
     loader.load(
-        "public/phoenix_bird.zip",
+        "public/" + obj_to_render + "/scene.gltf",
         function (gltf) {
-            scene.add(gltf.scene);
+            object = gltf.scene;
+            scene.add(object);
+
+            // Animation Mixer
+            mixer = new THREE.AnimationMixer(object);
+
+            // Play all animations
+            gltf.animations.forEach((clip) => {
+                mixer.clipAction(clip).play();
+            });
         },
-        undefined,
+        function (xhr) {
+            // Log loading of the object
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
         function (error) {
             console.error(error);
         }
     );
 
-    renderer.render(scene, camera);
+    // Adding lights
+    const top_light = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+    top_light.position.set(5000, 5000, 5000); // top-left-ish
+    top_light.castShadow = true;
+    scene.add(top_light);
+
+    const ambient_light = new THREE.AmbientLight(0x333333, 1);
+    scene.add(ambient_light);
+
+    // Add controls
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    // Render the scene
+    function animate() {
+        requestAnimationFrame(animate);
+
+        const delta = clock.getDelta();
+
+        if (mixer) {
+            mixer.update(delta);
+        }
+
+        renderer.render(scene, camera);
+    }
+
+    // Listener for resizing
+    window.addEventListener("resize", function () {
+        camera.aspect = this.window.innerWidth / this.window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(this.window.innerWidth, this.window.innerHeight);
+    });
+
+    animate();
 } else {
     const warning = WebGL.getWebGLErrorMessage();
+    console.log(warning);
     document.getElementById("container").appendChild(warning);
 }
